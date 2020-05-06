5ea581852b0b1630c3dd9996b7158e9fce4a71fb
Edvard <43088094+Lirurin@users.noreply.github.com>
2020-05-06 15:50:27 +0300

    svg load and animation added

--- app/js/main.js
+++ app/js/main.js
@@ -1,79 +1,163 @@
 document.addEventListener('DOMContentLoaded', function() {
-  let buildingArea = document.querySelector('.droppable')
+  // variables
+  let bricks = document.querySelectorAll('.bricks__block')
+  let buildingArea = document.querySelector('.droppable');
+  let bricksPile = document.querySelector('.bricks__pile');
+  let building = document.querySelector('.building');
   let areaOvered = false;
-  let brickCount = 0;
-  brick.onmousedown = function(event) {
+  let brickCount = 10;
+  let pathsLength = [];
+  let path;
 
-    let shiftX = event.clientX - brick.getBoundingClientRect().left;
-    let shiftY = event.clientY - brick.getBoundingClientRect().top;
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
-    let buildingAreaParams = {
-      x1: buildingArea.getBoundingClientRect().left,
-      y1: buildingArea.getBoundingClientRect().top,
-      x2: buildingArea.getBoundingClientRect().left + buildingArea.getBoundingClientRect().width,
-      y2: buildingArea.getBoundingClientRect().top + buildingArea.getBoundingClientRect().height,
+  //svg load
+  function svgLoad() {
+    let xhr = new XMLHttpRequest;
+    xhr.open('get',`../img/${ getRandomInt(0, 3)}.svg`,true);
+    xhr.onreadystatechange = function(){
+      if (xhr.readyState != 4) return;
+      let svg = xhr.responseXML.documentElement;
+      svg = document.importNode(svg,true);
+      building.appendChild(svg);
+      getSvgPath()
+      dragInit()
+    };
+    xhr.send();
+  }
+  svgLoad()
+
+  function getSvgPath() {
+    path = document.querySelectorAll('.building svg path');
+    pathsLength = [];
+    for (let i = 0; i < path.length; i++) {
+      pathsLength.push(path[i].getTotalLength())
     }
-    let brickAreaParams = {
-      x1: brick.getBoundingClientRect().left,
-      y1: brick.getBoundingClientRect().top,
-      x2: brick.getBoundingClientRect().left + buildingArea.getBoundingClientRect().width,
-      y2: brick.getBoundingClientRect().top + buildingArea.getBoundingClientRect().height,
+  }
+
+  function dragInit() {
+    for (let i = 0; i<bricks.length; i++) {
+      dragNdrop(bricks[i])
     }
-
-    brick.style.position = 'absolute';
-    brick.style.zIndex = 1000;
-    document.body.append(brick);
-
-    moveAt(event.pageX, event.pageY);
-
-    function moveAt(pageX, pageY) {
-      brick.style.left = pageX - shiftX + 'px';
-      brick.style.top = pageY - shiftY + 'px';
+  }
+  
+  function addMaterialsToBuilding() {
+     for (let i = 0; i < path.length; i++) {
+      let currentLength = pathsLength[i];
+      path[i].style.stroke = '#000000',
+      path[i].style.strokeDasharray = currentLength + ' ' + currentLength;
+      path[i].style.strokeDashoffset = currentLength;
+      path[i].getBoundingClientRect();
+      path[i].style.transition = path[i].style.WebkitTransition =
+        'stroke-dashoffset 1s ease-in-out';
+      if (brickCount !== 0) {
+        path[i].style.strokeDashoffset = `${currentLength - currentLength/brickCount}`;
+      } else {
+        path[i].style.transition = path[i].style.WebkitTransition = 'fill 1s ease ';
+        path[i].style.fill = '#000000'
+      }
+      console.log('path', path)
+      console.log('currentLength', currentLength - currentLength/10*brickCount)
     }
+  }
 
-    function onMouseMove(event) {
-      brickAreaParams = {
+  function createBrick() {
+    let newBrick = document.createElement("div");
+    newBrick.innerHTML = "<div class='bricks__cont'>кирпич</div>";
+    newBrick.setAttribute('id', 'brick');
+    newBrick.setAttribute('class', 'bricks__block');
+    bricksPile.appendChild(newBrick);
+    dragNdrop(newBrick)
+    addMaterialsToBuilding()
+  }
+
+  // dragNdrop
+  function dragNdrop(brick) {
+    brick.onmousedown = function(event) {
+      let shiftX = event.clientX - brick.getBoundingClientRect().left;
+      let shiftY = event.clientY - brick.getBoundingClientRect().top;
+      let buildingAreaParams = {
+        x1: buildingArea.getBoundingClientRect().left,
+        y1: buildingArea.getBoundingClientRect().top,
+        x2: buildingArea.getBoundingClientRect().left + buildingArea.getBoundingClientRect().width,
+        y2: buildingArea.getBoundingClientRect().top + buildingArea.getBoundingClientRect().height,
+      }
+      let brickAreaParams = {
         x1: brick.getBoundingClientRect().left,
         y1: brick.getBoundingClientRect().top,
         x2: brick.getBoundingClientRect().left + brick.getBoundingClientRect().width,
         y2: brick.getBoundingClientRect().top + brick.getBoundingClientRect().height,
       }
-
-      if (((brickAreaParams.x1 >= buildingAreaParams.x1 && brickAreaParams.x1 < buildingAreaParams.x2) 
-      || (brickAreaParams.x2 < buildingAreaParams.x2 && brickAreaParams.x2 >= buildingAreaParams.x1))
-      && ((brickAreaParams.y1 >= buildingAreaParams.y1 && brickAreaParams.y1 < buildingAreaParams.y2) 
-      || (brickAreaParams.y2 < buildingAreaParams.y2 && brickAreaParams.y2 >= buildingAreaParams.y1))) {
-        enterDroppable(buildingArea);
-        areaOvered = true;
-      } else {
-        leaveDroppable(buildingArea);
-        areaOvered = false;
-      }
+  
+      brick.style.position = 'absolute';
+      brick.style.zIndex = 1000;
+      document.body.append(brick);
       moveAt(event.pageX, event.pageY);
-      brick.hidden = true;
-      brick.hidden = false;
-    }
-
-    document.addEventListener('mousemove', onMouseMove);
-    brick.onmouseup = function() {
-      if (areaOvered) {
-        brickCount += 1;
-        document.querySelector('.bricks__text--count').textContent = `Собрано ${brickCount}`;
+      function moveAt(pageX, pageY) {
+        brick.style.left = pageX - shiftX + 'px';
+        brick.style.top = pageY - shiftY + 'px';
       }
-      document.removeEventListener('mousemove', onMouseMove);
-      brick.onmouseup = null;
+  
+      function onMouseMove(event) {
+        brickAreaParams = {
+          x1: brick.getBoundingClientRect().left,
+          y1: brick.getBoundingClientRect().top,
+          x2: brick.getBoundingClientRect().left + brick.getBoundingClientRect().width,
+          y2: brick.getBoundingClientRect().top + brick.getBoundingClientRect().height,
+        }
+
+        // collision detection 
+        if (((brickAreaParams.x1 >= buildingAreaParams.x1 && brickAreaParams.x1 < buildingAreaParams.x2) 
+        || (brickAreaParams.x2 < buildingAreaParams.x2 && brickAreaParams.x2 >= buildingAreaParams.x1))
+        && ((brickAreaParams.y1 >= buildingAreaParams.y1 && brickAreaParams.y1 < buildingAreaParams.y2) 
+        || (brickAreaParams.y2 < buildingAreaParams.y2 && brickAreaParams.y2 >= buildingAreaParams.y1))) {
+          enterDroppable(buildingArea);
+          areaOvered = true;
+        } else {
+          leaveDroppable(buildingArea);
+          areaOvered = false;
+        }
+        moveAt(event.pageX, event.pageY);
+        brick.hidden = true;
+        brick.hidden = false;
+      }
+  
+      document.addEventListener('mousemove', onMouseMove);
+      brick.onmouseup = function() {
+        if (areaOvered) {
+          if (brickCount === 0) {
+            brickCount = 10;
+            building.removeChild(building.childNodes[0])
+            setTimeout(svgLoad(), 1)
+          } else {
+             brickCount -= 1;
+          }
+         
+          document.querySelector('.bricks__text--count').textContent = `Осталось собрать ${brickCount} кирпича`;
+          brick.remove()
+          createBrick()
+        }
+        document.removeEventListener('mousemove', onMouseMove);
+        brick.onmouseup = null;
+        leaveDroppable(buildingArea);
+      };
     };
-  };
-
-  function enterDroppable(elem) {
-    elem.classList.add("hovered")
+  
+    function enterDroppable(elem) {
+      elem.classList.add("hovered")
+    }
+  
+    function leaveDroppable(elem) {
+      elem.classList.remove("hovered")
+    }
+  
+    brick.ondragstart = function() {
+      return false;
+    };
   }
 
-  function leaveDroppable(elem) {
-    elem.classList.remove("hovered")
-  }
-
-  brick.ondragstart = function() {
-    return false;
-  };
 }, false);
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,170 @@
-!function c(r,u,d){function g(t,e){if(!u[t]){if(!r[t]){var n="function"==typeof require&&require;if(!e&&n)return n(t,!0);if(l)return l(t,!0);var i=new Error("Cannot find module '"+t+"'");throw i.code="MODULE_NOT_FOUND",i}var o=u[t]={exports:{}};r[t][0].call(o.exports,function(e){return g(r[t][1][e]||e)},o,o.exports,c,r,u,d)}return u[t].exports}for(var l="function"==typeof require&&require,e=0;e<d.length;e++)g(d[e]);return g}({1:[function(e,t,n){"use strict";document.addEventListener("DOMContentLoaded",function(){var l=document.querySelector(".droppable"),f=!1,s=0;brick.onmousedown=function(e){var n=e.clientX-brick.getBoundingClientRect().left,i=e.clientY-brick.getBoundingClientRect().top,t=l.getBoundingClientRect().left,o=l.getBoundingClientRect().top,c=l.getBoundingClientRect().left+l.getBoundingClientRect().width,r=l.getBoundingClientRect().top+l.getBoundingClientRect().height,u={x1:brick.getBoundingClientRect().left,y1:brick.getBoundingClientRect().top,x2:brick.getBoundingClientRect().left+l.getBoundingClientRect().width,y2:brick.getBoundingClientRect().top+l.getBoundingClientRect().height};function d(e,t){brick.style.left=e-n+"px",brick.style.top=t-i+"px"}function g(e){u={x1:brick.getBoundingClientRect().left,y1:brick.getBoundingClientRect().top,x2:brick.getBoundingClientRect().left+brick.getBoundingClientRect().width,y2:brick.getBoundingClientRect().top+brick.getBoundingClientRect().height},f=(u.x1>=t&&u.x1<c||u.x2<c&&u.x2>=t)&&(u.y1>=o&&u.y1<r||u.y2<r&&u.y2>=o)?(l.classList.add("hovered"),!0):(l.classList.remove("hovered"),!1),d(e.pageX,e.pageY),brick.hidden=!0,brick.hidden=!1}brick.style.position="absolute",brick.style.zIndex=1e3,document.body.append(brick),d(e.pageX,e.pageY),document.addEventListener("mousemove",g),brick.onmouseup=function(){f&&(s+=1,document.querySelector(".bricks__text--count").textContent="Собрано ".concat(s)),document.removeEventListener("mousemove",g),brick.onmouseup=null}},brick.ondragstart=function(){return!1}},!1)},{}]},{},[1]);
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+  // variables
+  var bricks = document.querySelectorAll('.bricks__block');
+  var buildingArea = document.querySelector('.droppable');
+  var bricksPile = document.querySelector('.bricks__pile');
+  var building = document.querySelector('.building');
+  var areaOvered = false;
+  var brickCount = 10;
+  var pathsLength = [];
+  var path;
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  } //svg load
+
+
+  function svgLoad() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', "../img/".concat(getRandomInt(0, 3), ".svg"), true);
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState != 4) return;
+      var svg = xhr.responseXML.documentElement;
+      svg = document.importNode(svg, true);
+      building.appendChild(svg);
+      getSvgPath();
+      dragInit();
+    };
+
+    xhr.send();
+  }
+
+  svgLoad();
+
+  function getSvgPath() {
+    path = document.querySelectorAll('.building svg path');
+    pathsLength = [];
+
+    for (var i = 0; i < path.length; i++) {
+      pathsLength.push(path[i].getTotalLength());
+    }
+  }
+
+  function dragInit() {
+    for (var i = 0; i < bricks.length; i++) {
+      dragNdrop(bricks[i]);
+    }
+  }
+
+  function addMaterialsToBuilding() {
+    for (var i = 0; i < path.length; i++) {
+      var currentLength = pathsLength[i];
+      path[i].style.stroke = '#000000', path[i].style.strokeDasharray = currentLength + ' ' + currentLength;
+      path[i].style.strokeDashoffset = currentLength;
+      path[i].getBoundingClientRect();
+      path[i].style.transition = path[i].style.WebkitTransition = 'stroke-dashoffset 1s ease-in-out';
+
+      if (brickCount !== 0) {
+        path[i].style.strokeDashoffset = "".concat(currentLength - currentLength / brickCount);
+      } else {
+        path[i].style.transition = path[i].style.WebkitTransition = 'fill 1s ease ';
+        path[i].style.fill = '#000000';
+      }
+
+      console.log('path', path);
+      console.log('currentLength', currentLength - currentLength / 10 * brickCount);
+    }
+  }
+
+  function createBrick() {
+    var newBrick = document.createElement("div");
+    newBrick.innerHTML = "<div class='bricks__cont'>кирпич</div>";
+    newBrick.setAttribute('id', 'brick');
+    newBrick.setAttribute('class', 'bricks__block');
+    bricksPile.appendChild(newBrick);
+    dragNdrop(newBrick);
+    addMaterialsToBuilding();
+  } // dragNdrop
+
+
+  function dragNdrop(brick) {
+    brick.onmousedown = function (event) {
+      var shiftX = event.clientX - brick.getBoundingClientRect().left;
+      var shiftY = event.clientY - brick.getBoundingClientRect().top;
+      var buildingAreaParams = {
+        x1: buildingArea.getBoundingClientRect().left,
+        y1: buildingArea.getBoundingClientRect().top,
+        x2: buildingArea.getBoundingClientRect().left + buildingArea.getBoundingClientRect().width,
+        y2: buildingArea.getBoundingClientRect().top + buildingArea.getBoundingClientRect().height
+      };
+      var brickAreaParams = {
+        x1: brick.getBoundingClientRect().left,
+        y1: brick.getBoundingClientRect().top,
+        x2: brick.getBoundingClientRect().left + brick.getBoundingClientRect().width,
+        y2: brick.getBoundingClientRect().top + brick.getBoundingClientRect().height
+      };
+      brick.style.position = 'absolute';
+      brick.style.zIndex = 1000;
+      document.body.append(brick);
+      moveAt(event.pageX, event.pageY);
+
+      function moveAt(pageX, pageY) {
+        brick.style.left = pageX - shiftX + 'px';
+        brick.style.top = pageY - shiftY + 'px';
+      }
+
+      function onMouseMove(event) {
+        brickAreaParams = {
+          x1: brick.getBoundingClientRect().left,
+          y1: brick.getBoundingClientRect().top,
+          x2: brick.getBoundingClientRect().left + brick.getBoundingClientRect().width,
+          y2: brick.getBoundingClientRect().top + brick.getBoundingClientRect().height
+        }; // collision detection 
+
+        if ((brickAreaParams.x1 >= buildingAreaParams.x1 && brickAreaParams.x1 < buildingAreaParams.x2 || brickAreaParams.x2 < buildingAreaParams.x2 && brickAreaParams.x2 >= buildingAreaParams.x1) && (brickAreaParams.y1 >= buildingAreaParams.y1 && brickAreaParams.y1 < buildingAreaParams.y2 || brickAreaParams.y2 < buildingAreaParams.y2 && brickAreaParams.y2 >= buildingAreaParams.y1)) {
+          enterDroppable(buildingArea);
+          areaOvered = true;
+        } else {
+          leaveDroppable(buildingArea);
+          areaOvered = false;
+        }
+
+        moveAt(event.pageX, event.pageY);
+        brick.hidden = true;
+        brick.hidden = false;
+      }
+
+      document.addEventListener('mousemove', onMouseMove);
+
+      brick.onmouseup = function () {
+        if (areaOvered) {
+          if (brickCount === 0) {
+            brickCount = 10;
+            building.removeChild(building.childNodes[0]);
+            setTimeout(svgLoad(), 1);
+          } else {
+            brickCount -= 1;
+          }
+
+          document.querySelector('.bricks__text--count').textContent = "\u041E\u0441\u0442\u0430\u043B\u043E\u0441\u044C \u0441\u043E\u0431\u0440\u0430\u0442\u044C ".concat(brickCount, " \u043A\u0438\u0440\u043F\u0438\u0447\u0430");
+          brick.remove();
+          createBrick();
+        }
+
+        document.removeEventListener('mousemove', onMouseMove);
+        brick.onmouseup = null;
+        leaveDroppable(buildingArea);
+      };
+    };
+
+    function enterDroppable(elem) {
+      elem.classList.add("hovered");
+    }
+
+    function leaveDroppable(elem) {
+      elem.classList.remove("hovered");
+    }
+
+    brick.ondragstart = function () {
+      return false;
+    };
+  }
+}, false);
+
+},{}]},{},[1]);
